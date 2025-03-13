f7f6cb38a158d91431a18eea00b19b47da27e794
Antonio Paolillo <apaolill@gmail.com>
2025-03-13 18:11:25 +0100

    Apply formatting
    Signed-off-by: Antonio Paolillo <apaolill@gmail.com>

--- src/test_main.py
+++ src/test_main.py
@@ -2,11 +2,13 @@ def add(a: int, b: int) -> int:
     """Returns the sum of two numbers."""
     return a + b
 
+
 def test_add():
     assert add(2, 3) == 5
     assert add(-1, 1) == 0
     assert add(0, 0) == 0
     assert add(0, 3) == 3
+
 
 if __name__ == "__main__":
     print(f"2 + 3 = {add(2, 3)}")
