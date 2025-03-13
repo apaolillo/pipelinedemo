e01fe081af66bbb3f1bf5e679e0474321ed82b08
Antonio Paolillo <apaolill@gmail.com>
2025-03-13 18:07:06 +0100

    Fix error
    Signed-off-by: Antonio Paolillo <apaolill@gmail.com>

--- src/test_main.py
+++ src/test_main.py
@@ -6,7 +6,7 @@ def test_add():
     assert add(2, 3) == 5
     assert add(-1, 1) == 0
     assert add(0, 0) == 0
-    assert add(0, 3) == 4
+    assert add(0, 3) == 3
 
 if __name__ == "__main__":
     print(f"2 + 3 = {add(2, 3)}")
